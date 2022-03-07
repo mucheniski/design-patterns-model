3cdd11ff9afbab3a02d9e3c4fc1664ae9dc4c406
Diego Mucheniski <mucheniski@gmail.com>
2022-03-07 14:20:02 -0300

    fist commit

---
+++ src/test/java/com/example/designpatternsmodel/DesignPatternsModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpatternsmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
